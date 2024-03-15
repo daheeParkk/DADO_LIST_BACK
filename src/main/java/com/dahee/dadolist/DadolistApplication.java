86a1fc0d3039ec7841d18967c37083a8c71e2be5
daheeParkk <qkrekgml7414@naver.com>
2024-03-15 22:56:59 +0900

    first commit

---
+++ src/main/java/com/dahee/dadolist/DadolistApplication.java
@@ -0,0 +1,13 @@
+package com.dahee.dadolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dahee/dadolist/DadolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahee.dadolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
